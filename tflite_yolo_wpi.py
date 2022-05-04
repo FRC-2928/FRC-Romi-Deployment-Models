c7f3628dc7a745b9e97e1ef27f73804b524c242a
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2022-05-03 22:31:57 -0700

    deployment updates

--- tflite_yolo_wpi.py
+++ tflite_yolo_wpi.py
@@ -62,7 +62,7 @@ class Tester:
     def __init__(self, config_parser):
         print("Initializing TFLite runtime interpreter")
         try:
-            model_path = "model.tflite"
+            model_path = "rapid-react.tflite"
             self.interpreter = tflite.Interpreter(model_path, experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
             self.hardware_type = "Coral Edge TPU"
         except:
@@ -70,37 +70,25 @@ class Tester:
             model_path = "rapid-react.tflite"
             print(model_path)
             self.interpreter = tflite.Interpreter(model_path)
-            self.hardware_type = "Unoptimized"
-            print(self.hardware_type)
+            self.hardware_type = "Raspberry Pi CPU"
+
+        print(self.hardware_type)
 
         self.interpreter.allocate_tensors()
+        self.input_detail = self.interpreter.get_input_details()[0]
+        print(f"Model input shape {self.input_detail['shape']}")
 
         ## Read the model configuration file
         print("Loading network settings")
         default_config_file = 'rapid-react-config.json'
         configPath = str((Path(__file__).parent / Path(default_config_file)).resolve().absolute())    
         self.model_config = ModelConfigParser(configPath)
+
         print(self.model_config.labelMap)
         print("Classes:", self.model_config.classes)
         print("Confidence Threshold:", self.model_config.confidence_threshold)
 
-        # print("Getting labels")
-        # parser = PBTXTParser("map.pbtxt")
-        # parser.parse()
-        # self.labels = parser.get_labels()
-
-        
-        # print("Connecting to Network Tables")
-        # ntinst = NetworkTablesInstance.getDefault()
-        # ntinst.startClientTeam(config_parser.team)
-        # ntinst.startDSClient()
-        # self.entry = ntinst.getTable("ML").getEntry("detections")
-
-        # self.coral_entry = ntinst.getTable("ML").getEntry("coral")
-        # self.fps_entry = ntinst.getTable("ML").getEntry("fps")
-        # self.resolution_entry = ntinst.getTable("ML").getEntry("resolution")
-        # self.temp_entry = []
-
+        # Start the camera
         print("Starting camera server")
         cs = CameraServer.getInstance()
         camera = cs.startAutomaticCapture()
@@ -115,21 +103,20 @@ class Tester:
 
         # Connect to WPILib Network Tables
         print("Connecting to Network Tables")
-        hardware_type = "USB Camera"
-        self.nt = WPINetworkTables(config_parser.team, hardware_type, self.model_config.labelMap)
+        self.nt = WPINetworkTables(config_parser.team, self.hardware_type, self.model_config.labelMap)
 
     def run(self):
         print("Starting mainloop")
         fps = 0.0
         tic = time.time()
         while True:
-            # Acquire frame and resize to expected shape [1xHxWx3]
+            # Acquire image frame 
             ret, frame_cv2 = self.cvSink.grabFrame(self.img)
             if not ret:
                 print("Image failed")
                 continue
 
-            # input
+            # Resize to expected shape [1xHxWx3] and input image into the model 
             scale = self.set_input(frame_cv2)
 
             # run inference
@@ -165,15 +152,6 @@ class Tester:
             fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
             tic = toc
 
-            # Put data to Network Tables
-            # self.entry.setString(json.dumps(self.temp_entry))
-            # self.temp_entry = []
-            # if self.frames % 100 == 0:
-            #     print("Completed", self.frames, "frames. FPS:", (1 / (time() - start)))
-            # if self.frames % 10 == 0:
-            #     self.fps_entry.setNumber((1 / (time() - start)))
-            # self.frames += 1
-
     def label_frame(self, frame, object_name, box, score, x_scale, y_scale):
         ymin, xmin, ymax, xmax = box
         score = float(score)
@@ -206,7 +184,7 @@ class Tester:
 
     def input_size(self):
         """Returns input image size as (width, height) tuple."""
-        _, height, width, _ = self.interpreter.get_input_details()[0]['shape']
+        _, height, width, _ = self.input_detail['shape']
         return width, height
 
     def set_input(self, frame):
@@ -218,9 +196,11 @@ class Tester:
         """
         width, height = self.input_size()
         h, w, _ = frame.shape
-        print(frame.shape)
-        new_img = np.reshape(cv2.resize(frame.astype('float32'), (320, 512)), (1, 320, 512, 3))
-        self.interpreter.set_tensor(self.interpreter.get_input_details()[0]['index'], np.copy(new_img))
+        print(f"Frame shape {frame.shape}")
+
+        new_img = np.reshape(cv2.resize(frame.astype('float32'), (width, height)), (1, width, height, 3))
+
+        self.interpreter.set_tensor(self.input_detail['index'], np.copy(new_img))
         return width / w, height / h
 
     def output_tensor(self, i):
